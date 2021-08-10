f2ccd13191b48fde1c6fcf21f275935a40c8a24a
babitabisht <babitabisht2018@gmail.com>
2021-08-10 13:38:07 +0530

    swagger issue resolved

--- src/main/java/com/jwtauth/jwtauth/config/SwaggerConfig.java
+++ src/main/java/com/jwtauth/jwtauth/config/SwaggerConfig.java
@@ -7,17 +7,17 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
+
 public class SwaggerConfig {
 
- /*   @Bean
+    @Bean
     public Docket swaggerConfiguration(){
               return new Docket(DocumentationType.SWAGGER_2)
                       .select()
                       .paths(PathSelectors.ant("/api/*"))
                       .apis(RequestHandlerSelectors.basePackage("com.jwtauth"))
                       .build();
-    }*/
+    }
     
 
 }
